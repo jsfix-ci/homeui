bd735f448792c2b9f46ae5b0303f7988246b3856
vdromanov <vdromanov@yahoo.com>
2022-04-07 16:33:49 +0300

    more debug to select-with-hidden-items

--- app/scripts/components/json-editor/select-with-hidden-items.js
+++ app/scripts/components/json-editor/select-with-hidden-items.js
@@ -9,8 +9,17 @@ function makeSelectWithHiddenItems () {
             console.log('Editor: select-with-hidden-items')
             super.build()
 
+            console.log('this: %o', this)
+
             var options = this.switcher_options = this.theme.getSwitcherOptions(this.input)
+            console.log('var options: %o', options)
+
+            console.log('this.schema: %o', this.schema)
+            console.log('this.schema.options: %o', this.schema.options)
+            console.log('this.enum_values: %o', this.enum_values)
+
             options.forEach((op, i) => {
+                console.log(op, i)
                 if (this.schema.options &&
                     this.schema.options.enum_hidden &&
                     this.schema.options.enum_hidden.includes(this.enum_values[i])) {
