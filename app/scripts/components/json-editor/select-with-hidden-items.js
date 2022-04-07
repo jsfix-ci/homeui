b781fc6772a8923c6936e2c435aa7dc5487fc84f
vdromanov <vdromanov@yahoo.com>
2022-04-08 00:40:45 +0300

    split choices-hiding to separate func

--- app/scripts/components/json-editor/select-with-hidden-items.js
+++ app/scripts/components/json-editor/select-with-hidden-items.js
@@ -5,21 +5,9 @@ import { JSONEditor } from "../../../3rdparty/jsoneditor";
 function makeSelectWithHiddenItems () {
     return class extends JSONEditor.defaults.editors["select"] {
 
-        build () {
-            console.log('Editor: select-with-hidden-items')
-            super.build()
-
-            console.log('this: %o', this)
-
+        hideFromChoices () {
             var options = this.switcher_options = this.theme.getSwitcherOptions(this.input)
-            console.log('var options: %o', options)
-
-            console.log('this.schema: %o', this.schema)
-            console.log('this.schema.options: %o', this.schema.options)
-            console.log('this.enum_values: %o', this.enum_values)
-
             options.forEach((op, i) => {
-                console.log(op, i)
                 if (this.schema.options &&
                     this.schema.options.enum_hidden &&
                     this.schema.options.enum_hidden.includes(this.enum_values[i])) {
@@ -28,27 +16,14 @@ function makeSelectWithHiddenItems () {
             })
         }
 
+        build () {
+            super.build()
+            hideFromChoices()
+        }
+
         onWatchedFieldChange () {
-            console.log('Editor: select-with-hidden-items')
             super.onWatchedFieldChange()
-
-            console.log('this: %o', this)
-
-            var options = this.switcher_options = this.theme.getSwitcherOptions(this.input)
-            console.log('var options: %o', options)
-
-            console.log('this.schema: %o', this.schema)
-            console.log('this.schema.options: %o', this.schema.options)
-            console.log('this.enum_values: %o', this.enum_values)
-
-            options.forEach((op, i) => {
-                console.log(op, i)
-                if (this.schema.options &&
-                    this.schema.options.enum_hidden &&
-                    this.schema.options.enum_hidden.includes(this.enum_values[i])) {
-                    op.hidden = 'hidden'
-                }
-            })
+            hideFromChoices()
         }
     }
 }
