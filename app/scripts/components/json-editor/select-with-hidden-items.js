6ccd314e574696ffb4732e2b52b81ab2be698c42
vdromanov <vdromanov@yahoo.com>
2022-04-07 22:56:19 +0300

    added dynamic hiding

--- app/scripts/components/json-editor/select-with-hidden-items.js
+++ app/scripts/components/json-editor/select-with-hidden-items.js
@@ -27,6 +27,29 @@ function makeSelectWithHiddenItems () {
                 }
             })
         }
+
+        onWatchedFieldChange () {
+            console.log('Editor: select-with-hidden-items')
+            super.onWatchedFieldChange()
+
+            console.log('this: %o', this)
+
+            var options = this.switcher_options = this.theme.getSwitcherOptions(this.input)
+            console.log('var options: %o', options)
+
+            console.log('this.schema: %o', this.schema)
+            console.log('this.schema.options: %o', this.schema.options)
+            console.log('this.enum_values: %o', this.enum_values)
+
+            options.forEach((op, i) => {
+                console.log(op, i)
+                if (this.schema.options &&
+                    this.schema.options.enum_hidden &&
+                    this.schema.options.enum_hidden.includes(this.enum_values[i])) {
+                    op.hidden = 'hidden'
+                }
+            })
+        }
     }
 }
 
